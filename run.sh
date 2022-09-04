385b2a5ec98a1c326e91965d720301aa400c7ed3
xyz600 <xyz600600@gmail.com>
2022-09-05 02:29:58 +0900

    update for new dataset

--- run.sh
+++ run.sh
@@ -9,14 +9,16 @@ cargo build --release --bin solver
 cp target/release/solver ./solver_bin
 
 # single experiment
-# parallel --progress --result result ./solver_bin -i {} -s 2 ::: `seq 1 ${SINGLE_MAX_ID}`
-# for i in `seq 1 ${MAX_ID}`; do
-#     cp result/1/${i}/stdout solution/${i}.txt
-# done
+dataset= `seq 1 $SINGLE_MAX_ID` `seq 36 40`
+echo $dataset
+parallel --progress --result result ./solver_bin -i {} -s 2 ::: $dataset
+for i in $dataset ; do
+    cp result/1/${i}/stdout solution/${i}.txt
+done
 
-# twin experiment
+twin experiment
 TWIN_START_ID=`expr ${SINGLE_MAX_ID} + 1`
-parallel --progress --result result ./solver_bin -i {} -s 3 -t ::: `seq ${TWIN_START_ID} ${TWIN_MAX_ID}`
+parallel --progress --result result ./solver_bin -i {} -s 5 -t ::: `seq ${TWIN_START_ID} ${TWIN_MAX_ID}`
 for i in `seq ${TWIN_START_ID} ${TWIN_MAX_ID}`; do
     cp result/1/${i}/stdout solution/${i}.txt
 done
