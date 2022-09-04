af93f7155d235d7cef338b07d73227c609da6007
xyz600 <xyz600600@gmail.com>
2022-09-04 10:05:53 +0900

    use only problem-id

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ MAX_ID=25
 cargo build --release --bin solver
 cp target/release/solver ./solver_bin
 
-parallel --progress --result result ./solver_bin -i dataset/{}.png ::: `seq 1 ${MAX_ID}`
+parallel --progress --result result ./solver_bin -i {} ::: `seq 1 ${MAX_ID}`
 
 for i in `seq 1 ${MAX_ID}`; do
     cp result/1/${i}/stdout solution/${i}.txt
