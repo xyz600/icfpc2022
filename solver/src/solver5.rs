cc4235a816708df48b192f7542badcfc9be3e46e
xyz600 <xyz600600@gmail.com>
2022-09-05 17:59:51 +0900

    embed solver2 in solver5

--- solver/src/solver5.rs
+++ solver/src/solver5.rs
@@ -1,6 +1,10 @@
-use crate::{solver6, solver8};
 use common::problem::*;
 use std::path::Path;
+
+use crate::{
+    common_solver::{calculate_divisor_list, detect_edge, solve_by_divisor},
+    solver2,
+};
 
 // 全て merge して、 solver2::solve と同じことをします
 pub fn solve(problem_id: usize, image: &Image) -> State {
@@ -58,6 +62,76 @@ pub fn solve(problem_id: usize, image: &Image) -> State {
         clone
     };
 
-    let pre_state = solver6::solve(problem_id, image);
-    apply_prestate(pre_state)
+    // コピペしないと、途中の解が invalid になるのでダメでした
+    const POS_THREASHOLD: usize = 50;
+
+    let mut best_state = state.clone();
+    let mut best_score = evaluate(image, &best_state);
+
+    {
+        // edge 検出して、パターン数が少なければやってみる
+        let (row_list, column_list) = detect_edge(image, 20.0);
+        if row_list.len() <= POS_THREASHOLD && column_list.len() <= POS_THREASHOLD {
+            eprintln!("trying edge based division");
+            eprintln!("row {:?}", row_list);
+            eprintln!("col {:?}", column_list);
+            let pre_state = solve_by_divisor(image, &row_list, &column_list);
+            let state = apply_prestate(pre_state);
+
+            let exact_score = evaluate(image, &state);
+            eprintln!("update: {} -> {}", best_score, exact_score);
+
+            if best_score > exact_score {
+                best_score = exact_score;
+                best_state = state;
+
+                StateWithScore {
+                    score: best_score,
+                    state: best_state.clone(),
+                }
+                .save_if_global_best(problem_id);
+            }
+        } else {
+            eprintln!("cannot solve with edge: row = {}, column = {}", row_list.len(), column_list.len());
+        }
+    }
+
+    {
+        let image_size = 400;
+        let step_list = calculate_divisor_list(image_size);
+        eprintln!("step_list: {:?}", step_list);
+
+        for step in step_list.into_iter() {
+            if image_size > POS_THREASHOLD * step {
+                eprintln!("skip because step is too small ... {}", step);
+                continue;
+            }
+            eprintln!("trying {}", step);
+
+            let mut column_list = vec![];
+            let mut row_list = vec![];
+
+            for i in (0..=image_size).step_by(step) {
+                column_list.push(i);
+                row_list.push(i);
+            }
+
+            let pre_state = solve_by_divisor(image, &row_list, &column_list);
+            let state = apply_prestate(pre_state);
+
+            let exact_score = evaluate(image, &state);
+            eprintln!("update: {} -> {}", best_score, exact_score);
+            if best_score > exact_score {
+                best_score = exact_score;
+                best_state = state;
+                StateWithScore {
+                    score: best_score,
+                    state: best_state.clone(),
+                }
+                .save_if_global_best(problem_id);
+            }
+        }
+    }
+
+    best_state
 }
