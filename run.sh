a649b7b43ea908d343276337fa8f18502d308b10
xyz600 <xyz600600@gmail.com>
2022-09-05 18:22:23 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ parallel --progress --result result ./solver_bin -i {} -s 6 ::: $dataset &
 
 # twin experiment
 dataset_twin=`seq 26 35`
-parallel --progress ./solver_bin -i {} -s 5 -t ::: $dataset_twin &
+parallel --progress ./solver_bin -i {} -s 9 -t ::: $dataset_twin &
 
 wait
 
