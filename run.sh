d2eb9c841eff2bae6b1927706bab3f2071889926
xyz600 <xyz600600@gmail.com>
2022-09-05 14:36:01 +0900

    fix

--- run.sh
+++ run.sh
@@ -11,11 +11,11 @@ cp target/release/solver ./solver_bin
 # single experiment
 dataset="`seq 4 25` `seq 36 40`"
 echo $dataset
-parallel --progress --result result ./solver_bin -i {} -s 6 ::: $dataset &
+parallel --progress --result result ./solver_bin -i {} -s 8 ::: $dataset &
 
 # twin experiment
 dataset_twin=`seq 26 35`
-parallel --progress ./solver_bin -i {} -s 8 -t ::: $dataset_twin &
+parallel --progress ./solver_bin -i {} -s 5 -t ::: $dataset_twin &
 
 wait
 
